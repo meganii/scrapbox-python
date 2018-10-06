9e1bab55c98167657d8dd26a95b9b963c917c16c
meganii <yuhei24@gmail.com>
2018-10-06 15:06:49 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='scrapbox-python',
+    version='0.0.1',
+    description='Scrapbox Python',
+    long_description=readme,
+    author='meganii',
+    author_email='tsubokawa.yuhei@gmail.com',
+    url='https://gitlab.com/meganii/scrapbox-python.git',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
