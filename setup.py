1dba99004a69947bbd9b19180968ad0e5b7cdea1
meganii <yuhei24@gmail.com>
2018-10-06 15:21:30 +0900

    update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='scrapbox_python',
-    version='0.0.1',
+    version='0.0.2',
     description='Scrapbox Python',
     long_description=readme,
     author='meganii',
